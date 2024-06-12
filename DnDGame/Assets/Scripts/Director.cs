ae231a4ac582eba3ee932cd496fc383d5e72f6f9
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-06-12 00:10:56 -0400

    Added basic Scene switching
    currently just has the 2D scene switch to the 3D scene after 5 seconds using a Director script and a timeline

---
+++ DnDGame/Assets/Scripts/Director.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class Director : MonoBehaviour
+{
+
+    public PlayableDirector timeline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Subscribe to timeline events
+        timeline.stopped += OnTimelineStopped;
+        timeline.played += OnTimelinePlayed;
+
+        // Start timeline
+        timeline.Play();
+    }
+
+    void OnTimelinePlayed(PlayableDirector director)
+    {
+        // Actions to perform when the timeline starts
+    }
+
+    void OnTimelineStopped(PlayableDirector director)
+    {
+        // Example scene change based on timeline end
+        ChangeScene("3D");
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        // Unload current scene and load the new one
+        StartCoroutine(LoadScene(sceneName));
+    }
+
+    System.Collections.IEnumerator LoadScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
