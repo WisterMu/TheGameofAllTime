5f5cf00c5cf29eac4856e1bffde38400e798988c
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-07-17 12:59:49 -0400

    (2D) Added managers, added changing text on click
    May have to change it to an event system instead of direct method calls eventually

---
+++ DnDGame/Assets/Scripts/InputDetector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+
+    public TextDisplay text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Mouse Button Clicked");
+            HandleLeftClick();
+        }
+    }
+
+    // Action to perform when left click is detected
+    void HandleLeftClick()
+    {
+        text.UpdateText("Testing updated text");
+    }
+}
---
+++ DnDGame/Assets/TextDisplay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI uiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText.text = "Initial text";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Call this method to change the text of this object
+    public void UpdateText(string newText)
+    {
+        uiText.text = newText;
+    }
+}
