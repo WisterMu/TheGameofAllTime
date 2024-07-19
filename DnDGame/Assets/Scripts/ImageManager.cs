2fae81532e0d5e2a88c4db9cac318911719d609c
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-07-19 01:51:14 -0400

    (2D) Setup for image switching
    Added an image manager to go with it

---
+++ DnDGame/Assets/Scripts/ImageManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageManager : MonoBehaviour
+{
+    public RawImage[] imageArray;   // array of images to manipulate
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
