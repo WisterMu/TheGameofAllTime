b37a0b5fa8ffd2d185c222f074ad26e4e1b0726b
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-07-19 02:11:52 -0400

    (2D) Added image swapping

--- DnDGame/Assets/Scripts/ImageManager.cs
+++ DnDGame/Assets/Scripts/ImageManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class ImageManager : MonoBehaviour
 {
     public RawImage[] imageArray;   // array of images to manipulate
+    public Texture2D[] imageLibrary;    // complete collection of images to swap between
+    RawImage test;
+    int imageIndex = 0;
+    int libraryIndex = 0;
+    int frameCounter = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +23,11 @@ public class ImageManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        frameCounter++;
+        if (frameCounter % 60 == 0)     // every 60 frames
+        {
+            imageArray[imageIndex].texture = imageLibrary[libraryIndex];
+            libraryIndex = (libraryIndex + 1) % imageLibrary.Count();
+        }
     }
 }
