e16f2c8944364ef5503aaa0e3760c303af3c85ea
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-07-25 19:40:47 -0400

    (2D) Script parsing improved
    Dialogue and commands are now basically timelines, they can be synced with each other and have buffer nulls for when nothing is happening. Commands are indicated by everything to the right of a "//"

--- DnDGame/Assets/Scripts/TextManager.cs
+++ DnDGame/Assets/Scripts/TextManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
+using System.Text;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,9 +11,11 @@ using UnityEngine.UI;
 public class TextManager : MonoBehaviour
 {
     public TextMeshProUGUI uiText;
+    public TextMeshProUGUI commandTextDebug;
     List<string> dialogueList = new List<string>();
     List<string> commandList = new List<string>();
     int dialogueIndex = 0;
+    int emptyLineCount = 0;
     public TextAsset dialogueInput;     // Drag and drop text file to load
 
     // Start is called before the first frame update
@@ -35,7 +39,8 @@ public class TextManager : MonoBehaviour
         if (string.IsNullOrWhiteSpace(newText))
         {
             // empty string can be used as a buffer action
-            Debug.Log("EMPTY LINE");
+            Debug.Log("EMPTY LINE " + emptyLineCount);
+            emptyLineCount++;
         }
         else
         {
@@ -46,12 +51,16 @@ public class TextManager : MonoBehaviour
     // Call this method to change the text using its internal list of strings
     public void UpdateText()
     {
+        // test display for commands to be synced with dialogue
+        commandTextDebug.text = commandList[dialogueIndex];
+
         string newText = dialogueList[dialogueIndex];
         // only updates the text when it's not empty
         if (string.IsNullOrWhiteSpace(newText))
         {
             // empty string can be used as a buffer action
-            Debug.Log("EMPTY LINE");
+            Debug.Log("EMPTY LINE " + emptyLineCount);
+            emptyLineCount++;
             dialogueIndex = (dialogueIndex + 1) % dialogueList.Count;   // iterate dialogue index, loop back if overflow
         }
         else
@@ -59,6 +68,8 @@ public class TextManager : MonoBehaviour
             uiText.text = newText;                                      // swaps text to next one in list
             dialogueIndex = (dialogueIndex + 1) % dialogueList.Count;   // iterate dialogue index, loop back if overflow
         }
+
+
     }
 
     // Parse a text file to load into the list of strings for the dialogue
@@ -69,43 +80,154 @@ public class TextManager : MonoBehaviour
         {
             string text = textFile.ToString();              // Convert TextAsset to string
             string[] textArray = text.Split("\n");          // Split file into separate lines
-            // Debug.Log(textArray);
-            // dialogueList = new List<string>(textArray);     // Convert array into list
+            // each entry in textArray should be a line in the text file
 
+            // string prevLine = null;
             foreach (string line in textArray)  // Check each line separately to see if it's a command or not
             {
-                // No newlines to worry about, thankfully
-                // foreach(char ch in line)
-                // {
-                //     if (ch == '\n')
-                //     {
-                //         Debug.Log("NEWLINE FOUND");
-                //     }
-                // }
+                // splits each line based on /
+                // left half of line before / is dialogue, right half of line is command
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    // empty line
+                    dialogueList.Add(null);
+                    commandList.Add(null);
+                }
+                else
+                {
+                    string formattedLine = line;
+                    if (!line.Contains("//"))     // dialogue by itself, no command
+                    {
+                        formattedLine = line + "//";     // add the slash to end for splitting to work
+                    }
+                    Debug.Log("LINE: " + formattedLine);
+                    string[] splitLine = formattedLine.Split("//");
+                    foreach (string test in splitLine)
+                    {
+                        Debug.Log("ITEM: " + test);
+                    }
+                    string dialogueLine = splitLine[0].Trim(), commandLine = splitLine[1].Trim();
 
+                    if (string.IsNullOrWhiteSpace(dialogueLine))
+                    {
+                        dialogueList.Add(null);
+                    }
+                    else
+                    {
+                        dialogueList.Add(dialogueLine);
+                    }
+
+                    if (string.IsNullOrWhiteSpace(commandLine))
+                    {
+                        commandList.Add(null);
+                    }
+                    else
+                    {
+                        commandList.Add(commandLine);
+                    }
+                }
+
+                // all of the stuff below is probably useless, I was overcomplicating things
+
+                /*
+                string trimmedLine = line.Trim();   // removes leading and ending whitespace
+                if (string.IsNullOrWhiteSpace(trimmedLine))
+                {
+                    Debug.Log("LINE: (EMPTY)");
+                }
+                else
+                {
+                    Debug.Log("LINE: " + trimmedLine);
+                }
+                
+                
                 // likely want to treat each list as a "timeline" for each action to perform in order, and in sync if needed
                 // this means we'll need empty / buffer actions
-                if (line[0] == '/')     // if the line starts with a slash (this is a command)
+                if (string.IsNullOrWhiteSpace(trimmedLine))
                 {
-                    commandList.Add(line);
-                } 
-                else 
-                {
-                    dialogueList.Add(line);
+                    // this line is empty
+                    // if previous line was a command, insert null into dialogue only
+                    if ((prevLine != null) && prevLine.StartsWith("//"))
+                    {
+                        dialogueList.Add(null);
+                        Debug.Log("EMPTY LINE AFTER COMMAND");
+                    }
+                    else
+                    {   
+                        // insert null into both
+                        commandList.Add(null);
+                        dialogueList.Add(null);
+                    }
+                    
                 }
+                else
+                {
+                    if (trimmedLine.StartsWith("//"))
+                    {
+                        // this is a command line
+                        commandList.Add(trimmedLine);
+                    } 
+                    else
+                    {
+                        // this is a dialogue line
+                        dialogueList.Add(trimmedLine);
+                        if ((prevLine != null) && prevLine.StartsWith("//")) 
+                        {
+                            // previous line was a command OR this is the first line
+                            // add nothing to sync up with command
+                        }
+                        else
+                        {
+                            // previous line was empty, add null
+                            commandList.Add(null);
+                        }
+                    }
+                }
+
+                prevLine = trimmedLine;     // keep track of previous line for logic purposes
+                */
             }
         }
 
         // print out the stored lines for debugging
-        foreach (string line in commandList)
+        for (int i = 0; i < commandList.Count(); i++)
         {
-            Debug.Log("COMMAND: " + line);
+            Debug.Log("COMMAND: " + commandList[i] + "\nDIALOGUE: " + dialogueList[i]);
         }
-        foreach (string line in dialogueList)
+
+        // should be equal
+        if (commandList.Count() != dialogueList.Count())
         {
-            Debug.Log("DIALOGUE: " + line);
+            Debug.Log("ERROR: COMMAND AND DIALOGUE ARE OUT OF SYNC.\nCOMMAND COUNT: " + commandList.Count()
+            + "\nDIALOGUE COUNT: " + dialogueList.Count());
+        }
+        else
+        {
+            Debug.Log("Command and Dialogue are synced!");
         }
 
     }
+
+    // Debug function for converting a normal string to a literal string (all special characters are escaped)
+    string ToLiteral(string input)
+    {
+        StringBuilder literal = new StringBuilder(input.Length);
+        foreach (char c in input)
+        {
+            switch (c)
+            {
+                case '\n': literal.Append(@"\n"); break;
+                case '\r': literal.Append(@"\r"); break;
+                case '\t': literal.Append(@"\t"); break;
+                case '\\': literal.Append(@"\\"); break;
+                case '\0': literal.Append(@"\0"); break;
+                case '\b': literal.Append(@"\b"); break;
+                case '\f': literal.Append(@"\f"); break;
+                default: literal.Append(c); break;
+            }
+        }
+        return literal.ToString();
+    }
+
 
 }
